45753fe44fd0207eec734bc99d3b6a06a36af2b0
andre-cabral <cabral.dev@gmail.com>
2015-10-20 08:51:10 -0200

    Arrow with 90 degrees correct commit. Git ignore commited.

---
+++ Assets/Scripts/Utilities/DontRotateObjectXAxisUsingLocalRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontRotateObjectXAxisUsingLocalRotation : MonoBehaviour {
+	
+	float startRotationX;
+	
+	void Awake () {
+		startRotationX = transform.localRotation.eulerAngles.x;
+	}
+	
+	void Update () {
+		transform.rotation = Quaternion.Euler(new Vector3(startRotationX, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z));
+	}
+}
